fa4dccebd194b894e29222c74bd86838e47a3dfb
sam-the-tutor <atwebembeiresamuel2020@gmail.com>
2023-03-07 16:49:23 +0300

    Identity login both local and on IC

--- src/identityDapp_frontend/src/index.js
+++ src/identityDapp_frontend/src/index.js
@@ -1,19 +1,113 @@
-import { identityDapp_backend } from "../../declarations/identityDapp_backend";
+import { identityDapp_backend, createActor, canisterId } from "../../declarations/identityDapp_backend";
+import {AuthClient} from "@dfinity/auth-client"
+import {Actor, HttpAgent} from "@dfinity/agent";
+import { Principal } from "@dfinity/principal";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
 
-  const name = document.getElementById("name").value.toString();
+let actor = identityDapp_backend;
 
-  button.setAttribute("disabled", true);
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await identityDapp_backend.greet(name);
 
-  button.removeAttribute("disabled");
+const loginButton = document.getElementById("login");
+loginButton.onclick = async (e) => {
+    e.preventDefault();
+ 
+  let authClient = await AuthClient.create();
 
-  document.getElementById("greeting").innerText = greeting;
+  if (await authClient.isAuthenticated()) {
+    handleAuthenticated(authClient);
+  } else {
+    await authClient.login({
+      identityProvider: process.env.II_URL,
+      onSuccess: () => {
+        handleAuthenticated(authClient);
+      },
+    });
+  }
+};
+
+
+
+async function handleAuthenticated(authClient) {
+  const identity = await authClient.getIdentity();
+  const userPrincipal = await identity._principal.toString();
+
+  const agent = new HttpAgent({identity})
+  console.log("Agent:",agent)
+  console.log("canister Id:",canisterId)
+  console.log(userPrincipal);
+
+  actor = createActor(canisterId, {
+       agentOptions:{
+            identity,
+         },
+       
+     });
 
   return false;
-});
+
+
+  // ReactDOM.render(
+  //   <App loggedInPrincipal={userPrincipal} />,
+  //   document.getElementById("root")
+  // );
+}
+
+
+
+const greetButton = document.getElementById("greet");
+greetButton.onclick = async (e) => {
+    e.preventDefault();
+
+    greetButton.setAttribute("disabled", true);
+
+    // Interact with backend actor, calling the greet method
+    const greeting = await actor.greet();
+    console.log("Agent func call", greeting)
+
+    greetButton.removeAttribute("disabled");
+
+    document.getElementById("greeting").innerText = greeting;
+
+    return false;
+};
+
+
+
+
+
+// const loginButton = document.getElementById("login");
+// loginButton.onclick = async (e) => {
+//     e.preventDefault();
+
+//     let authClient = await AuthClient.create();
+
+//     await new Promise((resolve) => {
+//         authClient.login({
+//             identityProvider: process.env.II_URL,
+//             onSuccess: resolve,
+//         });
+//     });
+
+//     // At this point we're authenticated, and we can get the identity from the auth client:
+//     const identity = authClient.getIdentity();
+//     console.log("identity object",identity)
+//     // Using the identity obtained from the auth client, we can create an agent to interact with the IC.
+//     const agent = new HttpAgent({identity});
+//     /**
+//      Using the interface description of our webapp, we create an actor that we use to call the service methods.
+//       We override the global actor, such that the other button handler will automatically use 
+//       the new actor with the Internet Identity provided delegation.
+//      **/
+//     actor = createActor(canisterId, {
+//         agentOptions:{
+//              agent,
+//          },
+       
+//     });
+
+//     return false;
+// };
+
+
+
